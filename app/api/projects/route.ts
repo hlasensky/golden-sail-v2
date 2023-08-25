e22f5548d46728ba6094fbd4ab9bcfa3633a0125
hlasensky <tomas.hlasensky@seznam.cz>
2023-08-25 10:22:02 +0200

    fiiix

--- app/api/projects/route.ts
+++ app/api/projects/route.ts
@@ -1,14 +1,12 @@
 import { NextResponse } from "next/server";
-import fs from "fs/promises"
+import fs from "fs/promises";
 import path from "path";
 
 export async function GET() {
-	const pathFile = path.join(__dirname.split("app")[0], "app", "lib", "projects.json");
-	const files = await fs.readdir(__dirname.split(".next")[0])
-	console.log("dirmaneeeeee",__dirname)
-	console.log(files)
+	const pathdir = path.join(process.cwd(), "lib");
+	console.log(pathdir);
 	try {
-		const file = await fs.readFile(pathFile);
+		const file = await fs.readFile(path.join(pathdir, "projects.json"));
 
 		const data = JSON.parse(file.toString());
 		return NextResponse.json({ data }, { status: 200 });
